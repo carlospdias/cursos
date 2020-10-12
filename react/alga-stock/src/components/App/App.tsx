383c793b3e60059c76415e1e901f572807929a68
Carlos Pereira Dias <carlospdiase@gmail.com>
2020-10-12 17:23:42 -0300

    :tada: Base de código do curso de React adquirido da AlgaWorks.
    Mudanças a serem submetidas:
    
     - new file: react/alga-stock/.gitignore
     - new file: react/alga-stock/README.md
     - new file: react/alga-stock/package-lock.json
     - new file: react/alga-stock/package.json
     - new file: react/alga-stock/public/favicon.ico
     - new file: react/alga-stock/public/index.html
     - new file: react/alga-stock/public/logo192.png
     - new file: react/alga-stock/public/logo512.png
     - new file: react/alga-stock/public/manifest.json
     - new file: react/alga-stock/public/robots.txt
     - new file: react/alga-stock/src/App.test.tsx
     - new file: react/alga-stock/src/components/App/App.css
     - new file: react/alga-stock/src/components/App/App.tsx
     - new file: react/alga-stock/src/components/App/index.ts
     - new file: react/alga-stock/src/components/Header/Header.css
     - new file: react/alga-stock/src/components/Header/Header.tsx
     - new file: react/alga-stock/src/components/Header/index.ts
     - new file: react/alga-stock/src/index.css
     - new file: react/alga-stock/src/index.tsx
     - new file: react/alga-stock/src/react-app-env.d.ts
     - new file: react/alga-stock/src/serviceWorker.ts
     - new file: react/alga-stock/src/setupTests.ts
     - new file: react/alga-stock/tsconfig.json

---
+++ react/alga-stock/src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="AlgaStock" />
+
+    </div>
+  );
+}
+
+export default App;
