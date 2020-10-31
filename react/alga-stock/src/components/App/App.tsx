3368ef8de75beeda0febd90b195ca080550f6edd
Carlos Pereira Dias <carlospdiase@gmail.com>
2020-10-30 22:53:09 -0300

    :sparkles: Aplica o componente 'Table'.

--- react/alga-stock/src/components/App/App.tsx
+++ react/alga-stock/src/components/App/App.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import Header from '../Header';
 import './App.css';
 
+
 import Button from "../../shared/Button";
 import Container from '../../shared/Container';
 import Input from "../../shared/Input";
+import Table from '../../shared/Table';
 
 function IconComponent(){
     return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="Lupa de busca" />
@@ -19,6 +21,7 @@ function App() {
               Alerta
           </Button>
           <Input label="Página Principal." />
+          <Table />
       </Container>
     </div>
   );
